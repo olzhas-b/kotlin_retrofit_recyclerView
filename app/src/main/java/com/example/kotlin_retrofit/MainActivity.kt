7871a43510601d39ba62606450c9deee792a3eac
qwerty321kz <46135477+qwerty321kz@users.noreply.github.com>
2019-11-22 18:29:26 +0600

    second try

--- app/src/main/java/com/example/kotlin_retrofit/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_retrofit/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    private fun addDataSet(){
+//sdafsavvdsvsdbfdbzsvzdssvs    private fun addDataSet(){
 //        val data = DataSource.createDataSet()
 //        recyclerAdapter.submitList(data)
 //    }
